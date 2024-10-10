df24cc46db171c48504b7bd345350cffa3468ed9
Yongwook <kyw9885@gmail.com>
2024-10-10 17:37:46 +0900

    [1010]

--- src/main/java/view/KYWPrintResult.java
+++ src/main/java/view/KYWPrintResult.java
@@ -11,30 +11,20 @@ public class KYWPrintResult {
 
         System.out.println("요청하신 전체 메뉴 조회 결과 입니다.");
 
-        for (EmployeeDTO menu : employeeList) {
-
-    public void printMenuList(List<EmployeeDTO> menuList) {
-
-        System.out.println("요청하신 전체 메뉴 조회 결과 입니다.");
-
-        for (EmployeeDTO menu : menuList) {
-
-            System.out.println(menu);
+        for (EmployeeDTO memberDTO : employeeList) {
+            System.out.println("memberDTO = " + memberDTO);
         }
-
-        System.out.println("===============================");
-
     }
-    public void printErrorMessage(String errorCode) {
+    public void printErrorMessage (String errorCode){
         String errorMessage = "";
 
-        switch (errorCode) {
-            case "employeeList" :
-                errorMessage = "직원 전체 조회에 실패하셨습니다...";
-                break;
-        }
+            switch (errorCode) {
+                case "employeeList":
+                    errorMessage = "직원 전체 조회에 실패하셨습니다...";
+                    break;
+            }
 
-        System.out.println(errorMessage);
+            System.out.println(errorMessage);
 
     }
 
